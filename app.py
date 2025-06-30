7733289fe23bcf3a12bb907b2d3d73cbdca3fba5
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2025-06-30 16:54:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Horácios e Curiácios", layout="centered")
-st.title("Jogo: Os Horácios e os Curiácios")
+# st.title("Jogo: Os Horácios e os Curiácios")
 
 with open("tabuleiro.html", "r", encoding="utf-8") as f:
     html = f.read()
